9b099e60a53e1a042bd9e9a9162d820651b36d9d
Bijan <bmmielke@gmail.com>
2022-02-20 21:18:33 +0100

    gibt alle Daten aus die oo Spec sind. Sensor 4868 genutzt

--- BMI284_Statistics.py
+++ BMI284_Statistics.py
@@ -2,14 +2,14 @@
 # Date :        13.02.2022
 # Description:  Script for statistical analysis of BMI284 sensors
 
-
+from contextlib import suppress
 from matplotlib import pyplot as plt
 import pandas as pd
 #from nptdms import TdmsFile
 
 dataPath = 'BMI284_DB.csv'
 conversionPath = 'BMI284_conversion.csv'
-Sensor = '0000H6K250004036038KY'
+Sensor = '0000H2MH42021035014KY'
 compareToRef = 0
 compareToAll = 0
 
@@ -29,22 +29,26 @@ data = faDB.loc[Sensor]
 #MeasMax = conv['Max']
 #MeasStd_max = conv['Std_max']
 
-#for Measurement in conv.iterrows():
-
-'''
-test if one Value can be converted and checked correctly before iterating over all values
-CV_D_wodrivewoCM
-'''
-para = conv.iloc[6]
-
-if data.size != 0:      #check if Sensor is listed in DB
+for index, Measurement in conv.iterrows():
+    para = conv.iloc[index]
     Meas = para['Measurement']
-    MeasMean = data[para['Mean']]
+    if 'AIAA' in Meas:
+        MeasMean = data['g:rate_x:CM_inSens_PD_AIAA_pos:mean'] - data['g:rate_x:CM_inSens_PD_AIAA_neg:mean']
+    elif 'AI' in Meas:
+        MeasMean = data['g:rate_x:CM_inSens_PD_AI_pos:mean'] - data['g:rate_x:CM_inSens_PD_AI_neg:mean']
+    elif 'AA' in Meas:
+        MeasMean = data['g:rate_x:CM_inSens_PD_AA_pos:mean'] - data['g:rate_x:CM_inSens_PD_AA_neg:mean']
+    elif 'S31' in Meas:
+        MeasMean = data['g:rate_x:CM_in_Sens_pos:mean'] - data['g:rate_x:CM_in_Sens_neg:mean']
+    else:    
+        MeasMean = data[para['Mean']]
     ConvMean = MeasMean * para['Conversion']
+    if 'CV_D_in_Sens' == Meas:
+        ConvMean = abs(ConvMean)
     if ConvMean > para['Min'] and ConvMean < para['Max']:
-        print(f'{Meas} = {ConvMean} : is in Spec')
+        #print(f'{Meas} = {ConvMean} : is in Spec')
+        continue
+    elif ConvMean < para['Min']:
+        print(f'{Meas} = {ConvMean} : is too low')
     else:
-        print(f'{Meas} = {ConvMean} : is out of Spec')
-    
-else:
-    print('ERROR: USNR not found')
+        print(f'{Meas} = {ConvMean} : is too high') 
